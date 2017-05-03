373b856f3203a7eb94fa631d998138ed1791bbc6
priom <priom.galaxy@gmail.com>
2017-05-03 03:21:47 -0400

    stripe config

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   mount_uploader :avatar, AvatarUploader
+
+  has_one :payment
+  accepts_nested_attributes_for :payment
 end
