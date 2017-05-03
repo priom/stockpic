7a98ba6d7ef0dd3367be7893ada5eefc9acd2d61
priom <priom.galaxy@gmail.com>
2017-05-03 03:55:58 -0400

    cleanup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails5starter_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "stockpic#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -85,5 +85,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'rails5starter.herokuapp.com', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => 'stockpic.herokuapp.com', :protocol => 'https' }
 end
